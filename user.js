56a0ab1d94a9c2c8c01bb0a258e1bd00479eba0d
Chonlada666 <chonlada.nu@ku.th>
2025-03-31 15:44:29 +0700

    CSS Update

--- user.js
+++ user.js
@@ -1,25 +1,21 @@
 const BASE_URL = 'http://localhost:8000'
-
-
 window.onload = async () => {
-  await loadData()
+    await loadData()
 }
 
 const loadData = async () => {
-  console.log('user page loaded')
-  //1. load user ทั้งหมด จาก api ที่เตรียมไว้
-  const response = await axios.get(`${BASE_URL}/users`)
+    console.log('users page loaded');
+    //1.load user ทั้งหมด จาก api ที่เตรียม
+    const response = await axios.get(`${BASE_URL}/users`)
+    console.log(response.data)
 
-  console.log(response.data)
+    const userDOM = document.getElementById('users')
+    //2.นำ user ทั้งหมด โหลดกลับเข้าไปhtml
 
-  const userDOM = document.getElementById('user')
-  //2. นำ user ทั้งหมด โหลดกลับเข้าไปใน html
- 
-  //สร้างตารางเพื่อแสดงข้อมูล user
-  let htmlData = `
-  <table border="1" cellspacing="1" cellpadding="10">
-      <thead>
-          <tr>
+    let htmlData = `
+    <table border="1">
+        <thead>
+            <tr>
               <th>ID</th>
               <th>Firstname</th>
               <th>Lastname</th>
@@ -28,15 +24,16 @@ const loadData = async () => {
               <th>Interests</th>
               <th>Description</th>
               <th>Action</th>
-          </tr>
-      </thead>
-      <tbody>
-  `;
+            </tr>
+        </thead>
+        <tbody>
+    `;
 
-  for (let i = 0; i < response.data.length; i++) {
-    let users = response.data[i];
-    htmlData += `
-      <tr>
+    
+    for(let i = 0; i < response.data.length; i++) {
+      let users = response.data[i]
+      htmlData += `
+        <tr>
           <td>${users.id}</td>
           <td>${users.firstname}</td>
           <td>${users.lastname}</td>
@@ -44,31 +41,31 @@ const loadData = async () => {
           <td>${users.gender}</td>
           <td>${users.interests || '-'}</td>
           <td>${users.description || '-'}</td>
-          <td>
-          <a href="index.html?id=${users.id}"><button class='Edit'>Edit</button></a>
-          <button class="delete" data-id="${users.id}">Delete</button>
-          </td>
-      </tr>
-    `;
-  }
+            <td>
+                <a href='index.html?id=${users.id}'><button>Edit</button></a>
+                <button class='delete' data-id='${users.id}'>Delete</button>
+            </td>
+        </tr>
+        `
+    }
+    htmlData += '</div>'
+    userDOM.innerHTML = htmlData
 
-  htmlData += `
-            </tbody>
-        </table>
-    `;
-    userDOM.innerHTML = htmlData;
-
-  //3. สร้าง event สำหรับลบ user
-  const deletDOMs = document.getElementsByClassName('delete')
-  for (let i = 0; i < deletDOMs.length; i++) {
-    deletDOMs[i].addEventListener('click', async (event) => {
-      const id = event.target.dataset.id
-      try {
-        await axios.delete(`${BASE_URL}/users/${id}`)
-        loadData()//เรียกใช้ฟังก์ชั่นตัวเองเพื่อโหลดข้อมูลใหม่
-      } catch (error) {
-        console.log('error', error)
-      }
-    })
-  }
+    //3. ลบ user
+    const deleteDOMs = document.getElementsByClassName('delete')
+    for (let i = 0; i < deleteDOMs.length; i++) {
+        deleteDOMs[i].addEventListener('click', async (event) => {
+            //ดึก id ของ user ที่ต้องการลบ
+            const id = event.target.dataset.id
+            try{
+                await axios.delete(`${BASE_URL}/users/${id}`)
+                loadData() //recursive function = เรียกใช้ฟังก์ชัน ตัวเอง
+            }catch (error) {
+                console.log('error',error)
+            }
+        })
+        document.getElementById("backButton").addEventListener("click", function() {
+            window.location.href = "index.html";
+        });        
+    }
 }
